00f8ca0444c9b3ef4f42279ba61682d9816734cf
Pablo <peyjo3@gmail.com>
2024-10-15 11:35:25 +0200

    list all nad findbyid

--- src/main/java/bank/controller/BankAccountController.java
+++ src/main/java/bank/controller/BankAccountController.java
@@ -3,10 +3,9 @@ package bank.controller;
 import bank.model.BankAccount;
 import bank.service.BankAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bankAccount")
@@ -18,5 +17,15 @@ public class BankAccountController {
     @PostMapping("/create")
     private BankAccount createAccount(@RequestBody BankAccount bankAccount){
         return this.bankAccountService.createAccount(bankAccount);
+    }
+
+    @GetMapping("/")
+    private List<BankAccount> getAllAccount(){
+        return this.bankAccountService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    private BankAccount getAccount(@PathVariable Long id){
+        return this.bankAccountService.findById(id);
     }
 }
